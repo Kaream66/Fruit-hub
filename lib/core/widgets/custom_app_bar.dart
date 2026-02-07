373b7a806a68452c5f7613937f6e3bb1142016c9
Kaream <kareamtanany@gmail.com>
2026-02-07 14:17:05 +0200

    ..

--- lib/core/widgets/custom_app_bar.dart
+++ lib/core/widgets/custom_app_bar.dart
@@ -3,6 +3,7 @@ import 'package:fruits_hub/core/utils/app_text_styles.dart';
 
 AppBar CustomAppBar(context, {required String title}) {
   return AppBar(
+    backgroundColor: Colors.white,
     centerTitle: true,
     title: Text(title, style: TextStyles.bold19),
     leading: IconButton(
