81edc18f0876c8b8d1e605ef0318a60c63fcc575
Kaream <kareamtanany@gmail.com>
2026-06-28 09:08:38 +0300

    create sign up cubit and state

---
+++ lib/features/auth/presentation/manager/cubits/cubit/sign_up_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:bloc/bloc.dart';
+import 'package:fruits_hub/features/auth/domain/entities/user_entity.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:meta/meta.dart';
+
+part 'sign_up_state.dart';
+
+class SignUpCubit extends Cubit<SignUpState> {
+  SignUpCubit(this.authRepo) : super(SignUpInitial());
+  final AuthRepo authRepo;
+  Future<void> createUserWithEmailAndPassword(String email, String password, String name) async {
+    emit(SignUpLoading());
+    final result = await authRepo.createUserWithEmailAndPassword(email, password, name);
+    result.fold(
+      (failures) => emit(SignUpFailure(errorMessage: failures.message)),
+      (userEntity) => emit(SignUpSuccess(userEntity: userEntity)),
+    );
+  }
+}
