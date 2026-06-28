8b1ecaef92d8dc77d78ed90c4aecdd13583570bd
Kaream <kareamtanany@gmail.com>
2026-06-28 12:50:48 +0300

    add get it to sign up cubit and add bloc provider at sign up view

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:fruits_hub/core/services/getit_service.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/domain/repos/auth_repo.dart';
+import 'package:fruits_hub/features/auth/presentation/manager/cubits/cubit/sign_up_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/widgets/signup_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,9 +11,12 @@ class SignUpView extends StatelessWidget {
   static const routeName = 'signup';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(context, title: 'حساب جديد'),
-      body: SignupViewBody(),
+    return BlocProvider(
+      create: (context) => SignUpCubit(getIt<AuthRepo>()),
+      child: Scaffold(
+        appBar: CustomAppBar(context, title: 'حساب جديد'),
+        body: SignupViewBody(),
+      ),
     );
   }
 }
